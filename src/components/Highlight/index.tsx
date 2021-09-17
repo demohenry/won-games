f05ecf2f5bd4540d3c75f2d822a6591a4f2e134c
Democrito <demohenry6@gmail.com>
2021-09-17 09:54:52 -0300

    Highlight completed

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -6,18 +6,23 @@ export type HighlightProps = {
 	title: string;
 	subtitle: string;
 	buttonLabel: string;
+	floatImage?: string;
 	backgroundImage: string;
 	buttonLink: string;
+	alignment?: 'right' | 'left';
 };
 
 const Highlight = ({
 	title,
 	subtitle,
 	backgroundImage,
+	floatImage,
 	buttonLabel,
 	buttonLink,
+	alignment = 'right',
 }: HighlightProps) => (
-	<S.Wrapper backgroundImage={backgroundImage}>
+	<S.Wrapper backgroundImage={backgroundImage} alignment={alignment}>
+		{!!floatImage && <S.FloatImage src={floatImage} alt={title} />}
 		<S.Content>
 			<S.Title>{title}</S.Title>
 			<S.Subtitle>{subtitle}</S.Subtitle>
